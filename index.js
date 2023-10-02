45d868aac628218410c6638c9d77e1364fe02299
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-10-02 14:20:03 +0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42); 
+  }
+
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264; 
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const distanceInBlocks = Math.abs(destination - start);
+    return distanceInBlocks * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0; // First 400 feet are free
+    } else if (distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+    } else if (distance < 2500) {
+      return 25; // Flat fare for distances between 2000 and 2500 feet
+    } else {
+      return 'cannot travel that far'; // Over 2500 feet
+    }
+  }
